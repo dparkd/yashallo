c114a3a1faa781ea326aa8203ba61604f66b4ce7
Daniel Park <dpark@Daniels-MacBook-Pro-2.local>
2016-08-27 01:01:58 -0400

    Automated commit at 2016-08-27 05:01:53 UTC by middleman-deploy 1.0.0

---
+++ javascripts/all.js
@@ -0,0 +1,57 @@
+$(document).ready(function() {
+  var w = Math.max(document.documentElement.clientWidth, window.innerWidth || 0)
+  var h = Math.max(document.documentElement.clientHeight, window.innerHeight || 0)
+
+  //Set the height of the element
+  $('.header').css('height', h);
+  $('.header .brand-title').css('padding-top', h/2.4);
+
+
+  $('#close-about').click(function() {
+    $('.about, .about-img').removeClass('show');
+    $('.about, .about-img').addClass('hide');
+  });
+
+  $('#open-about').click(function() {
+    $('.about, .about-img').addClass('show');
+    $('.about, .about-img').removeClass('hide');
+  });
+
+  var sticky_navigation_offset_top = $('.norway .navigation').offset().top;
+  var sticky_navigation = function(){
+    var scroll_top = $(window).scrollTop(); 
+    if (scroll_top > sticky_navigation_offset_top) { 
+      $('.norway .navigation').addClass('fixed');
+    } else {
+      $('.norway .navigation').removeClass('fixed');
+    }   
+  };
+  
+  // run our function on load
+  sticky_navigation();
+  
+  // and run it again every time you scroll
+  $(window).scroll(function() {
+    sticky_navigation();
+  });
+
+  // lazy load images 
+  $(function() {
+    $(".lazy").lazyload();
+  });
+
+
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
